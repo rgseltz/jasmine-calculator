393d0a8d76e87f24b5ae758eb1a47ff40bb38a56
ryan <ryanjgecelter@gmail.com>
2022-11-01 13:50:13 +0200

    test calculateMonthlyPayment fn

--- calculator-test.js
+++ calculator-test.js
@@ -1,10 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = { principal: 100000, years: 10, rate: 4 }
+  expect(calculateMonthlyPayment(values)).toBe('1012.45');
 });
 
 
-it("should return a result with 2 decimal places", function() {
+it("should return a result with 2 decimal places", function () {
   // ..
 });
 
