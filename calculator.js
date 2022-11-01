cb2df4975172c254cb554668d182cef3cdf1bd84
ryan <ryanjgecelter@gmail.com>
2022-11-01 11:53:27 +0200

    added UI dom funcs

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -21,17 +21,28 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = { principal: 100000, years: 10, rate: 4 };
+  const principalUI = document.getElementById('loan-amount');
+  principalUI.value = values.principal;
+  const yearsUI = document.getElementById('loan-years');
+  yearsUI.value = values.years;
+  const rateUI = document.getElementById('loan-rate');
+  rateUI.value = values.rate;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const currentUIValues = getCurrentUIValues();
+  calculateMonthlyPayment(currentUIValues);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+
 }
 
 // Given a string representing the monthly payment value,
